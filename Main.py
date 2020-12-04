105bb52511ad5dab05d950cf3e045d3be4e70513
Federico <zabalero@gmail.com>
2020-12-04 20:24:23 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,19 @@
+SYNTAX
+
+
+\	escape character. Print the character and don't interpret it
+_	_23variable (used for variables starting with a number)
+Variables can be ProperCase
+
+
+#	Line comment
+"""	Multiple line comment. Needs to be closed with another """
+
+# PyCharm Color Scheme
+''' => Text
+""" => String
+'   => String text
+Text
+
+
+
